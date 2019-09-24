12bf7e76c8097e8a0426cf69ad828b2e41e8d6c1
Anton Lukichev <anton@lukichev.pro>
2019-09-24 15:35:21 +0300

    upd

---
+++ src/firebase/config_sample.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'ENTER HERE',
+  authDomain: 'ENTER HERE',
+  databaseURL: 'ENTER HERE',
+  projectId: 'ENTER HERE',
+  storageBucket: 'ENTER HERE',
+  messagingSenderId: 'ENTER HERE',
+  appId: 'ENTER HERE',
+};
+
+export default firebaseConfig;
